6f22841bff021566dc1bb8ca131a56a3ffba6322
CosmomediaCEO <93375686+CosmomediaCEO@users.noreply.github.com>
2024-06-28 08:43:28 +0200

    Update SHQStripeBundle.php

--- src/SHQStripeBundle.php
+++ src/SHQStripeBundle.php
@@ -66,8 +66,7 @@ class SHQStripeBundle extends Bundle
         return DoctrineOrmMappingsPass::createXmlMappingDriver(
             $mappings,
             ['stripe_bundle.model_manager_name'],
-            'stripe_bundle.backend_orm',
-            ['SHQStripeBundle' => 'SerendipityHQ\Bundle\StripeBundle\Model']
+            'stripe_bundle.backend_orm'
         );
     }
 }
